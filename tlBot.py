353660de4e3a9fbf72142058f926987b20aca882
thuxeko <nguyenhathu2594@gmail.com>
2021-04-23 14:33:09 +0700

    a

--- tlBot.py
+++ tlBot.py
@@ -36,7 +36,7 @@ def getToken(update: Update, context: CallbackContext) -> None:
     bot.send_message(chat_id=update.effective_message.chat_id,
                      text=userMes, parse_mode='MarkdownV2')
     
-    print(bot._message)
+    print(update.message)
     # update.message.delete()
 
 def getContract(update: Update, context: CallbackContext) -> None:
