244824aaba52d128bf76cdc7ceb2b0ea9f4272c4
ThuXeko <nguyenhathu2594@gmail.com>
2021-04-24 01:40:52 +0700

    a

--- tlBot.py
+++ tlBot.py
@@ -25,17 +25,20 @@ logger = logging.getLogger(__name__)
 def getToken(update: Update, context: CallbackContext) -> None:
     token = update.message.text.replace('/p', '').strip()
     if token:
-        strOut = checkbsc.getTokenWithSymbol(
-            token, 1, update.message.from_user['first_name'], update.message.from_user['id'])
-        print(strOut)
-        # update.message.reply_html(strOut)
-        mesoutbybot = bot.send_message(chat_id=update.effective_message.chat_id,
-                                       text=strOut, parse_mode='HTML')
-        print('Message ID Bot: ' + mesoutbybot['message_id'] + ' - Chat ID Bot: ' +
-              mesoutbybot['chat']['id'] + ' - Time: ' + mesoutbybot['date'])
-        utils.updateChat(
-            mesoutbybot['message_id'], mesoutbybot['chat']['id'], mesoutbybot['date'])
-        update.message.delete()
+        try:
+            strOut = checkbsc.getTokenWithSymbol(
+                token, 1, update.message.from_user['first_name'], update.message.from_user['id'])
+            # update.message.reply_html(strOut)
+            mesoutbybot = bot.send_message(chat_id=update.effective_message.chat_id,
+                                           text=strOut, parse_mode='HTML')
+            print('Message ID Bot: ' + mesoutbybot['message_id'] + ' - Chat ID Bot: ' +
+                  mesoutbybot['chat']['id'] + ' - Time: ' + mesoutbybot['date'])
+            utils.updateChat(
+                mesoutbybot['message_id'], mesoutbybot['chat']['id'], mesoutbybot['date'])
+            update.message.delete()
+        except Exception as e:
+            print(e)
+
     else:
         update.message.reply_text("Sai câu lệnh")
     # print(update.message)
