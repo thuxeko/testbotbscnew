05a34912268a39d645bb2756b54acf0cdd1b728a
thuxeko <nguyenhathu2594@gmail.com>
2021-04-23 09:27:40 +0700

    update

--- tlConfig/credentials.py
+++ tlConfig/credentials.py
@@ -1,4 +1,4 @@
-bot_token = "1791583661:AAH8RapLCbSAMrqLxs5vkREEzTY18c1oYPQ"
-bot_user_name = "bscchecktxkt_bot"
-URL = "https://bscchecktxkt.herokuapp.com/"
+bot_token = "1574629803:AAE8FgoEOn4IidjdbkLT_lZOKscr1UGLhIo"
+bot_user_name = "testbscnew_bot"
+URL = "https://bscbottest.herokuapp.com/"
 
