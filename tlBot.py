2fc4f9b75bb9a724171b3d9ca37ea85123151c4f
thuxeko <nguyenhathu2594@gmail.com>
2021-04-29 15:07:15 +0700

    Update ăn xin

--- tlBot.py
+++ tlBot.py
@@ -1,12 +1,13 @@
 # Lib
-from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, Bot, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 import os
 import logging
 import time
+import json
 
 # Config
-from tlConfig.credentials import bot_token, URL, typeGroup
+from tlConfig.credentials import bot_token, URL, typeGroup, typePerson
 
 # Include
 import checkbsc
@@ -15,6 +16,8 @@ import utils
 TOKEN = bot_token
 bot = Bot(TOKEN)
 timeConfig = 60
+
+SWL = range(1)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -147,6 +150,63 @@ def getContract2(update: Update, context: CallbackContext) -> None:
 #endregion
 
 
+#region An xin
+def setWallet(update: Update, _: CallbackContext) -> int:
+    typeChat = update.message['chat']['type']
+    if typeChat == typePerson:
+        update.message.reply_text(
+            'Vui lòng nhập nội dung ví theo định dạng bạn mong muốn\nVí dụ: Ủng hộ quỹ hưu trí: \nVí BNB: xxxx\nVí ETH: xxx\nNhập /cancel để huỷ bỏ', reply_markup=ReplyKeyboardRemove())
+            
+        return SWL
+    else:
+        update.message.reply_text('Group giàu bỏ mẹ éo cho set ví nhớ 🤨🤨🤨')
+        return ConversationHandler.END
+
+
+def SaveWL(update: Update, _: CallbackContext) -> int:
+    with open('save_wallet.json', 'r') as dl_file:
+        data = json.load(dl_file)
+        user = [x for x in data if x['user_id'] == update.message.from_user.id]
+
+    if (len(user) > 0):
+        user[0]['mes_user'] = update.message['text']
+        with open('save_wallet.json', 'w') as data_file:
+            json.dump(data, data_file)
+    else:
+        objUser = {
+            'user_id': update.message.from_user.id,
+            'mes_user': update.message['text']
+        }
+
+        data.append(objUser)
+        with open('save_wallet.json', 'w') as data_file:
+            json.dump(data, data_file)
+    
+    update.message.reply_text('Nhập thông tin thành công')
+    return ConversationHandler.END
+
+def cancel(update: Update, _: CallbackContext) -> int:
+    return ConversationHandler.END
+
+def anxin(update: Update, context: CallbackContext) -> None:
+    with open('save_wallet.json', 'r') as dl_file:
+        data = json.load(dl_file)
+        user = [x for x in data if x['user_id'] == update.message.from_user.id]
+        if (len(user) > 0):
+            update.message.reply_text(user[0]['mes_user'])
+        else:
+            update.message.reply_text('Giàu vl ăn xin gì nữa')
+    # update.message.reply_text(
+    #     'Ủng hộ cha làm bot nghèo 🙇🏻 \nBNB-Bep20: 0x810Caa3fFf0A9C56b22B745dE713d1b305dDbA71\nETH: 0x6067Eb0f98AB2488d8AB66232CeCCdc399c94A0D'
+    # )
+#endregion
+
+#region Other
+def chaosep(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        'Chúng em chào sếp Hạnh, sếp Hạnh vạn tuế vạn tuế vạn vạn tuế 🙇 🙇 🙇'
+    )
+
 def infoBot(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         'Em chào anh, nhà em 3 đời làm bot 🤖, hiện tại bot này nhà em có: \n/p - Nhập mã Token để lấy thông tin v1\n/ct - Nhập mã Token để lấy Contract v1\n/p2 - Nhập mã Token để lấy thông tin v2\n/ct2 - Nhập mã Token để lấy Contract v2\n/info - Gọi em ra để dooddeed nè'
@@ -155,16 +215,7 @@ def infoBot(update: Update, context: CallbackContext) -> None:
 
 def error(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
-
-def anxin(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Ủng hộ cha làm bot nghèo 🙇🏻 \nBNB-Bep20: 0x810Caa3fFf0A9C56b22B745dE713d1b305dDbA71\nETH: 0x6067Eb0f98AB2488d8AB66232CeCCdc399c94A0D'
-    )
-
-def chaosep(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Chúng em chào sếp Hạnh, sếp Hạnh vạn tuế vạn tuế vạn vạn tuế 🙇 🙇 🙇'
-    )
+#endregion
 
 def main():
     try:
@@ -180,6 +231,15 @@ def main():
         dp.add_handler(CommandHandler("info", infoBot))
         dp.add_handler(CommandHandler("anxin", anxin))
         dp.add_handler(CommandHandler("chaosep", chaosep))
+
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('setwl', setWallet)],
+            states={
+                SWL: [MessageHandler(Filters.text, SaveWL)]
+            },
+            fallbacks=[CommandHandler('cancel', cancel)],
+        )
+        dp.add_handler(conv_handler)
 
         # Log error
         dp.add_error_handler(error)
--- utils.py
+++ utils.py
@@ -1,13 +1,5 @@
 import json
 
-# def delete_oldTime():
-#     with open('delete_save.json', 'r') as dl_file:
-#         data = json.load(dl_file)
-#         # list_delete = list(filter(lambda x: x["time"] <= 1619163730, data))
-#         list_save = list(filter(lambda x: x["time"] > 1619163730, data))
-#         save_delete_file(list_save)
-
-
 def save_delete_file(lstsave):
     with open('delete_save.json', 'w') as data_file:
         json.dump(lstsave, data_file)
